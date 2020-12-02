ac1bfaacfee3b081686926cc85b39435f51140f1
hadiazb <hadiazb@unal.edu.co>
2020-12-02 18:52:34 -0500

    [ADD] test list

---
+++ src/Typescript/Fila_cola.ts
@@ -0,0 +1,72 @@
+// Fila / Cola
+
+class Queue {
+	items: any;
+	front: number;
+	end: number;
+	constructor() {
+		this.items = {};
+		this.front = 0;
+		this.end = 0;
+	}
+
+	enqueue(data: number | string | boolean) {
+		this.items[this.end] = data;
+		this.end++;
+	}
+
+	dequeue() {
+		if (this.front === this.end) {
+			return null;
+		}
+
+		const data = this.items[this.front];
+		delete this.items[this.front];
+		this.front++;
+		return data;
+	}
+
+	getSize() {
+		return this.end - this.front;
+	}
+
+	isEmpty() {
+		if (this.getSize() === 0) {
+			return true;
+		}
+		return false;
+	}
+
+	peek() {
+		if (this.getSize() === 0) {
+			return null;
+		}
+		return this.items[this.front];
+	}
+
+	print() {
+		if (this.getSize() === 0) {
+			return null;
+		}
+		let result = '[';
+		for (let i = this.front; i < this.end; i++) {
+			if (i + 1 !== this.end) {
+				result += this.items[i] + ', ';
+			} else {
+				result += this.items[i] + ']';
+			}
+		}
+		return result;
+	}
+}
+
+const queue: Queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue('Soy la roca');
+queue.enqueue(true);
+queue.dequeue();
+
+console.log(queue.print());
---
+++ src/Typescript/Pila_stack.ts
@@ -0,0 +1,80 @@
+// Lista ordenada o estructura de datos  que permite almacenar y recuperar
+// datos, el modo de acceso a sus elementos  es de tipo LIFO
+
+class Stack {
+	items: any;
+	top: number;
+	constructor() {
+		this.items = {};
+		this.top = 0;
+	}
+
+	push(data: any) {
+		this.top++;
+		this.items[this.top] = data;
+	}
+
+	pop() {
+		let deletedData;
+		if (this.top) {
+			deletedData = this.items[this.top];
+			delete this.items[this.top];
+			this.top--;
+			return deletedData;
+		}
+		return null;
+	}
+
+	getSize() {
+		return this.top;
+	}
+
+	isEmpty() {
+		if (!this.getSize()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	peek() {
+		if (this.isEmpty()) {
+			return null;
+		}
+		return this.items[this.top];
+	}
+
+	print() {
+		if (this.getSize() === 0) {
+			return null;
+		}
+		let result = '[';
+		for (let i = this.top; i > 0; i--) {
+			if (i !== 1) {
+				result += this.items[i] + ', ';
+			} else {
+				result += this.items[i] + ']';
+			}
+		}
+		return result;
+	}
+}
+
+const stack: Stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(5);
+stack.push(5);
+stack.push(5);
+stack.push(8);
+stack.push(10);
+
+console.log('Stack =>', stack);
+console.log('Tamaño =>', stack.getSize());
+console.log('Valor Eliminado =>', stack.pop());
+console.log('Stack =>', stack);
+console.log('isEmpty', stack.isEmpty());
+console.log('Peek =>', stack.peek());
+console.log('Print =>', stack.print());
